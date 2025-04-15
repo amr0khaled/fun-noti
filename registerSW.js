b1f280fe46afa27415185484547c4ec9b6662bdf
Amr Khaled <redwolf4325@gmail.com>
2025-04-15 16:02:17 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa.sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa.sw.js', { scope: '/fun-noti' })})}
